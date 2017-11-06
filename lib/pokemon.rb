f404985dd9a48260d22c17309b6cc03addabe808
Kristy Peterson <kpeterson.kap@gmail.com>
2017-11-06 00:19:47 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+
+
+attr_accessor :id, :name, :type, :db
+
+def initialize(id:, name:, type:, db:)
+  @id = id
+  @name = name
+  @type = type
+  @db = db
+end
+
+def self.save(name, type, db)
+  db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+end
+
+# The find method creates a new Pokemon after selecting their row from the database by their id number.
+def self.find()
+
+
+  
+end
+  
+
 end
